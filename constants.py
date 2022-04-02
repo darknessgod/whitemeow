7618f101615187d33c03040e35a0be42f993bb61
Tianyi Pu <912396513@qq.com>
2022-04-02 16:36:42 +0100

    add adjacent
    输入行、列、1维下标、行数、列数，返回相邻格的1维下标

--- constants.py
+++ constants.py
@@ -6,6 +6,29 @@ ELEMENT_PATH = TEXTURE_PATH + "elements/"
 defaultsettings={'showplayertag':True,'defaultplayertag':'请按F5设置标识','playername':'anonymous',
 'level1name':'beg','level2name':'int','level3name':'exp','level4name':'cus','enablerec':True }
 
+def adjacent(i,j,index,row,column):
+    if j==0: # left edge
+        if i==0: # top-left corner
+            return (index+1,index+row,index+row+1)
+        elif i==row-1: # bottom-left corner
+            return (index-row,index-row+1,index+1)
+        else:
+            return (index-row,index-row+1,index+1,index+row,index+row+1)
+    elif j==column: # right edge
+        if i==0: # top-right corner
+            return (index-1,index+row-1,index+row)
+        elif i==row-1: # bottom-right corner
+            return (index-row-1,index-row,index-1)
+        else:
+            return (index-row-1,index-row,index-1,index+row-1,index+row)
+    else:
+        if i==0: # top edge
+            return (index-1,index+1,index+row-1,index+row,index+row+1)
+        elif i==row-1: # bottom edge
+            return (index-row-1,index-row,index-row+1,index-1,index+1)
+        else:
+            return (index-row-1,index-row,index-row+1,index-1,index+1,index+row-1,index+row,index+row+1)
+
 class smallfuc(object):#放一些静态方法
     @staticmethod
     def linyu(i,j,r,c):
